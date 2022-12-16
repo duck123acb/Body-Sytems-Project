85796f6000c84bae796de9e1670c663b782d1047
Danush Ramanan <dan.ramanan@gmail.com>
2022-12-16 08:06:49 -0700

    basic game system

---
+++ script.js
@@ -0,0 +1,34 @@
+// elements
+const img = document.getElementById("img");
+const input = document.getElementById("input");
+const messageUI = document.getElementById("message");
+const moveOn = document.getElementById("move-on");
+
+// questions
+const imgs = [""]; // img srcs
+const answers = [""]; // answers to each question
+let questionIndex;
+
+function getInput() {
+	const answer = input.value;
+	input.value = "";
+	return answer;
+}
+
+input.addEventListener("keydown", (event) => {
+	if (event.key !== "Enter") return;
+	
+	const answer = getInput;
+	if (answer === answers[questionIndex]) {
+		messageUI.innerText = "Correct!";
+		moveOn.style.display = "block";
+		questionIndex++;
+	}
+	else messageUI.innerText = "Wrong, try again."
+});
+
+moveOn.addEventListener("click", () => {
+	moveOn.style.display = "none";
+	messageUI.innerText = "What is the above picture showing?"
+	img.src = answers[questionIndex];
+});
